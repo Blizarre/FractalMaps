dfb5af29f6645001eda777e90f0e1c02325b1803
Simon M <webaccount@marache.net>
2014-07-10 19:59:05 +0200

    First version of the ImageExplorer Module

---
+++ ImageExplorer/Explorer.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+
+namespace ImageExplorer
+{
+    public partial class Explorer : FrameworkElement
+    {
+
+        Dictionary<Point, BitmapSource> tileCache = new Dictionary<Point, BitmapSource>();
+        Size tileSize = new Size(128, 128);
+        Point currentPosition = new Point(0, 0);
+        RandomImageGenerator rm;
+        int numberOfRendering = 0;
+
+        public Point? posMouseDown = null;
+
+        protected override void OnMouseDown(MouseButtonEventArgs e)
+        {
+            posMouseDown = new Point( e.GetPosition(this) );
+        }
+
+
+        protected override void OnMouseUp(MouseButtonEventArgs e)
+        {
+            posMouseDown = null;
+        }
+
+        protected override void OnMouseEnter(MouseEventArgs e)
+        {
+            if(e.LeftButton == MouseButtonState.Released)
+                posMouseDown = null;
+        }
+
+
+        protected override void OnMouseMove(MouseEventArgs e)
+        {
+            if (e.LeftButton == MouseButtonState.Pressed && posMouseDown != null)
+            {
+                Point nouveauPoint = new Point(e.GetPosition(this));
+                Point difference = new Point();
+                difference.X = nouveauPoint.X - posMouseDown.Value.X;
+                difference.Y = nouveauPoint.Y - posMouseDown.Value.Y;
+                posMouseDown = nouveauPoint;
+                this.currentPosition.X -= difference.X;
+                this.currentPosition.Y -= difference.Y;
+                this.InvalidateVisual();
+                //posMouseDown = nouveauPoint; // int truncating will likely cause some problems
+            }
+        }
+
+        protected override HitTestResult HitTestCore(PointHitTestParameters hitTestParameters)
+        {
+            return new PointHitTestResult(this, hitTestParameters.HitPoint);
+        }
+
+
+        public Explorer()
+        {
+            rm = new RandomImageGenerator(this.tileSize);
+        }
+
+        protected override void OnRender(DrawingContext context)
+        {
+            DateTime begin = DateTime.Now;
+            System.Windows.Size current = this.RenderSize;
+            // cast to int : truncate
+            int drawingAreaWidth = (int)(current.Width / tileSize.Width) + 2;
+            int drawingAreaHeight = (int)(current.Width / tileSize.Width) + 2;
+            Rect rectangle= new Rect();
+            BitmapSource toDraw;
+
+            for(int i = -1; i < drawingAreaWidth; i++)
+            {
+                for(int j = -1; j < drawingAreaHeight; j++)
+                {
+                    
+                    Point upperLeftPoint = new Point(currentPosition.X + i * tileSize.Width, currentPosition.Y + j * tileSize.Height);
+                    Point upperLeftPointNormalized = new Point(tileSize.Width * (int)(upperLeftPoint.X / tileSize.Width), tileSize.Height * (int)(upperLeftPoint.Y / tileSize.Height));
+
+                    if(tileCache.ContainsKey(upperLeftPointNormalized))
+                    {
+                        toDraw = tileCache[upperLeftPointNormalized];
+                    }
+                    else
+                    {
+                        toDraw = rm.generate(upperLeftPointNormalized);
+                        tileCache[upperLeftPointNormalized] = toDraw;
+                    }
+                    rectangle.Location = new System.Windows.Point(upperLeftPointNormalized.X - this.currentPosition.X, upperLeftPointNormalized.Y - this.currentPosition.Y);
+                    rectangle.Width = tileSize.Width;
+                    rectangle.Height = tileSize.Height;
+                    context.DrawImage(toDraw, rectangle);
+                }
+            }
+
+            DateTime end = DateTime.Now;
+
+            FormattedText formattedText = new FormattedText(
+                "RenderingTime:" + (end - begin).Milliseconds + "ms.",
+                CultureInfo.GetCultureInfo("fr-fr"),
+                FlowDirection.LeftToRight,
+                new Typeface("Verdana"),
+                20,
+                Brushes.Blue
+            );
+            context.DrawText(formattedText, new System.Windows.Point(20, 20));
+
+        }
+
+    }
+}
---
+++ ImageExplorer/RandomImageGenerator.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+
+namespace ImageExplorer
+{
+    public struct Size
+    {
+        public Size(int w, int h)
+        {
+            this.Width = w;
+            this.Height = h;
+        }
+
+        public String toString()
+        {
+            return "Width:" + this.Width + ", Height:" + this.Height;
+        }
+        
+        public bool Equals(Size s)
+        {
+            return (this.Width == s.Width && this.Height == s.Height);
+        }
+
+        public int Width;
+        public int Height;
+    }
+
+    public struct Point
+    {
+        public Point(int x, int y)
+        {
+            this.X = x;
+            this.Y = y;
+        }
+
+        public Point(System.Windows.Point point)
+        {
+            this.X = (int)point.X;
+            this.Y = (int)point.Y;
+        }
+
+
+        public String toString()
+        {
+            return "X:" + this.X + ", X:" + this.Y;
+        }
+
+        public int X;
+        public int Y;
+    }
+
+
+    class RandomImageGenerator
+    {
+        Size m_s;
+
+        public RandomImageGenerator(Size size)
+        {
+            m_s = size;
+        }
+
+        public BitmapSource generate(Point origin)
+        {
+            // Define parameters used to create the BitmapSource.
+            PixelFormat pf = PixelFormats.Bgr32;
+            int width = this.m_s.Width;
+            int height = this.m_s.Height;
+            int rawStride = (width * pf.BitsPerPixel) / 8;
+            byte[] rawImage = new byte[rawStride * height];
+
+            // Initialize the image with data.
+            for (int i = 0; i < rawImage.Length; i++)
+            {
+                rawImage[i] = (byte)((Math.Sin(origin.X) * 255 + Math.Sin(origin.Y) * 255) % Byte.MaxValue);
+            }
+            
+            // Create a BitmapSource.
+            BitmapSource bitmap = BitmapSource.Create(width, height,
+                96, 96, pf, null,
+                rawImage, rawStride);
+
+            return bitmap;
+        }
+
+    }
+}
