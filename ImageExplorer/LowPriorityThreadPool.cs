cd534ac13646f0450ad6f2f21b58599624c6cb28
Simon M. <webaccount@marache.net>
2014-07-18 20:16:55 +0200

    First commit of the threading object

---
+++ ImageExplorer/LowPriorityThreadPool.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ImageExplorer
+{
+    class LowPriorityThreadPool
+    {
+        List<Thread> threadpool;
+
+        
+        public LowPriorityThreadPool(int number, ThreadStart start)
+        {
+            this.threadpool = new List<Thread>();
+            for(int i = 0; i < number; i++)
+            {
+                threadpool.Add(new Thread(start));
+            }
+        }
+
+
+    }
+}
